ac7455d1b131c420f77a4ef21225c6db32883f92
pavkriz <pavkriz@hfree.org>
2023-11-16 09:22:47 +0100

    D3 dragging, highlight neighbours

--- src/D3Renderer.tsx
+++ src/D3Renderer.tsx
@@ -4,6 +4,8 @@ import {MapModel} from "./org/hkfree/ospf/model/map/MapModel";
 import {LinkEdge} from "./org/hkfree/ospf/model/map/impl/LinkEdge";
 import {IVertex} from "./org/hkfree/ospf/model/map/IVertex";
 
+//  https://spin.atomicobject.com/2017/07/20/d3-react-typescript/
+
 interface FDGNode extends d3.SimulationNodeDatum {
     id: string;
     group: number;
@@ -24,7 +26,9 @@ export function D3Renderer(props: { mapModel: MapModel}) {
 
         const graph = d3.select(".d3-container")
             .attr("width", width)
-            .attr("height", height);
+            .attr("height", height)
+            // remove all old children
+            .selectAll("*").remove();
 
         const links: FDGLink[] = props.mapModel.getLinkEdges().toArray<LinkEdge>()
             .filter((link: LinkEdge) => link.getVertex1() && link.getVertex2() && link.getVertex1()?.getLabel() && link.getVertex2()?.getLabel()) // TODO - remove this filter
@@ -45,8 +49,35 @@ export function D3Renderer(props: { mapModel: MapModel}) {
             }
         });
 
+        //const circleDrag: d3Drag.DragBehavior<SVGCircleElement, CircleDatum, CircleDatum | d3Drag.SubjectPosition>
+
         console.log('links', links);
         console.log('nodes', nodes);
+
+        const drag = (simulation: d3.Simulation<FDGNode, FDGLink>) => {
+
+            function dragstarted(event: { active: any; subject: { fx: any; x: any; fy: any; y: any; }; }) {
+                if (!event.active) simulation.alphaTarget(0.3).restart();
+                event.subject.fx = event.subject.x;
+                event.subject.fy = event.subject.y;
+            }
+
+            function dragged(event: { subject: { fx: any; fy: any; }; x: any; y: any; }) {
+                event.subject.fx = event.x;
+                event.subject.fy = event.y;
+            }
+
+            function dragended(event: { active: any; subject: { fx: null; fy: null; }; }) {
+                if (!event.active) simulation.alphaTarget(0);
+                event.subject.fx = null;
+                event.subject.fy = null;
+            }
+
+            return d3.drag<SVGCircleElement, FDGNode>()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended);
+        }
 
         // Create the SVG container.
         const svg = d3.select(".d3-container")
@@ -80,10 +111,41 @@ export function D3Renderer(props: { mapModel: MapModel}) {
             .join("circle")
             .attr("r", 5)
             //.attr("fill", d => color(d.group));
-            .attr("fill", "#f00");
+            .attr("fill", "#f00")
+            // @ts-ignore
+            .call(drag(simulation))
+            .on("mouseenter", (evt, d) => {
+                link
+                    .attr("display", "none")
+                    // @ts-ignore
+                    .filter(l => l.source.id === d.id || l.target.id === d.id)
+                    .attr("display", "block");
+            })
+            .on("mouseleave", evt => {
+                link.attr("display", "block");
+            });
+
+        // const nodeLabels = var text = svg.a
+        //     .data(data).enter().append("text")
+        //     .attr("x", function(d) {
+        //         return d.x;
+        //     })
+        //     .attr("y", function(d) {
+        //         return d.y;
+        //     })
+        //     .attr("dx",12)
+        //     .attr("dy",".35em")
+        //     .text(function(d){
+        //         return d.objectName;
+        //     });
 
         node.append("title")
             .text(d => d.id);
+
+        // node.append("text")
+        //     .text(function(d) { return d.id; })
+        //     .attr("x", 6)
+        //     .attr("y", 3);
 
         function ticked() {
             console.log("ticked");
@@ -97,6 +159,8 @@ export function D3Renderer(props: { mapModel: MapModel}) {
                 .attr("cx", d => d.x!)
                 .attr("cy", d => d.y!);
         }
+
+
 
 
     });
